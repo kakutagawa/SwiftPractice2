8faf5c19bfe8a0828ed6730c1af5b5c4db634e05
kakutagawa <kaktgw0824@gmail.com>
2023-10-09 14:40:33 +0900

    Initial Commit

---
+++ SwiftPractice/SwiftPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPracticeApp.swift
+//  SwiftPractice
+//
+//  Created by 芥川浩平 on 2023/10/09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
