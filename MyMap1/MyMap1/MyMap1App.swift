eab5e3a435bda0dfc55c31c29082ef300ee5ece2
kakutagawa <kaktgw0824@gmail.com>
2023-10-12 00:17:48 +0900

    MyMap1というアプリを作ったよ

---
+++ MyMap1/MyMap1/MyMap1App.swift
@@ -0,0 +1,17 @@
+//
+//  MyMap1App.swift
+//  MyMap1
+//
+//  Created by 芥川浩平 on 2023/10/11.
+//
+
+import SwiftUI
+
+@main
+struct MyMap1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
