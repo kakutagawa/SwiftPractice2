fbbe5b3dc1604409f3335c1f894ab492fba82f8e
kakutagawa <kaktgw0824@gmail.com>
2023-10-13 08:28:16 +0900

    privateつけたり型省略をしたりしたよ

--- MyMap1/MyMap1/ContentView.swift
+++ MyMap1/MyMap1/ContentView.swift
@@ -8,34 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
-//入力中の文字列を保持する状態変数
-    @State var inputText: String = ""
-//検索キーワードを保持する状態変数
-    @State var displaySearchKey: String = ""
-//マップ種類　最初は標準から
-    @State var displayMapType: MapType = .standard
+    //入力中の文字列を保持する状態変数
+    @State private var inputText = ""
+    //検索キーワードを保持する状態変数
+    @State private var displaySearchKey = ""
+    //マップ種類　最初は標準から
+    @State private var displayMapType: MapType = .standard
     
     var body: some View {
-//垂直方向レイアウト
+        //垂直方向レイアウト
         VStack {
-//テキストフィールド
+            //テキストフィールド
             TextField("キーワード", text: $inputText, prompt: Text("キーワードを入力してください"))
-//入力が完了されたとき
+            //入力が完了されたとき
                 .onSubmit {
-//入力が完了したので検索キーワードに設定する
+                    //入力が完了したので検索キーワードに設定する
                     displaySearchKey = inputText
                 }
-//余白の追加
+            //余白の追加
                 .padding()
             
-//奥から手前方向にレイアウト（右下基準で配置）
+            //奥から手前方向にレイアウト（右下基準で配置）
             ZStack(alignment: .bottomTrailing) {
-//マップを表示
+                //マップを表示
                 MapView(searchKey: displaySearchKey, mapType: displayMapType)
                 
-//マップ表示切り替えボタン
+                //マップ表示切り替えボタン
                 Button {
-//標準→衛星写真→衛星写真＋交通機関ラベル
+                    //標準→衛星写真→衛星写真＋交通機関ラベル
+                    switch displayMapType {
+                    case .standard:
+                        
+                    case .satellite:
+                        
+                    case .hybrid:
+                    }
+                    
                     if displayMapType == .standard {
                         displayMapType = .satellite
                     } else if displayMapType == .satellite {
@@ -44,14 +52,14 @@ struct ContentView: View {
                         displayMapType = .standard
                     }
                 } label: {
-//マップアイコンの表示
+                    //マップアイコンの表示
                     Image(systemName: "map")
                         .resizable()
                         .frame(width: 35.0, height: 35.0)
                 }
-//右の余白を20開ける
+                //右の余白を20開ける
                 .padding(.trailing, 20.0)
-//下の余白を30開ける
+                //下の余白を30開ける
                 .padding(.bottom, 30.0)
                 
             }
