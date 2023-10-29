1e8a5bb223696ff851c354f6910833c73d13c199
kakutagawa <kaktgw0824@gmail.com>
2023-10-29 13:09:44 +0900

    MyOkashi1完成（教科書の内容全て）

--- MyOkashi1/MyOkashi1/ContentView.swift
+++ MyOkashi1/MyOkashi1/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     @StateObject var okashiDataList = OkashiData()
     //入力された文字列を保持する状態変数
     @State var inputText = ""
+    //SafariViewの表示有無を管理する変数
+    @State var showSafari = false
 
     var body: some View {
         //垂直レイアウト
@@ -28,6 +30,42 @@ struct ContentView: View {
             .submitLabel(.search)
             //上下左右に空白
             .padding()
+
+            //リスト表示する
+            List(okashiDataList.okashiList) { okashi in
+                //１つ１つの要素を取り出す
+                //ボタンを用紙
+                Button {
+                    //選択したリンクを保存する
+                    okashiDataList.okashiLink = okashi.link
+                    //SafariViewを表示
+                    showSafari.toggle()
+                } label: {
+                    //Listの表示内容を生成する
+                    //水平方向レイアウト
+                    HStack {
+                        //画像を読み込み、表示する
+                        AsyncImage(url: okashi.image) { image in
+                            //画像を表示する
+                            image
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 40)
+                        } placeholder: {
+                            //読み込み中はインジケーターを表示
+                            ProgressView()
+                        }
+                        //テキスト表示
+                        Text(okashi.name)
+                    }//HStack
+                }//Button
+            }//List
+            .sheet(isPresented: $showSafari, content: {
+                //SafariViewを表示
+                SafariView(url: okashiDataList.okashiLink!)
+                //画面下部がセーフエリア外までいっぱいになるように指定
+                    .ignoresSafeArea(edges: [.bottom])
+            }) //sheet
         }//VStack
     }// body
 }// ContentView
